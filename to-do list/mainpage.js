d646bde5aa70501a3be2592fce90d95fe59b9931
Larshjelen <56083146+Larshjelen@users.noreply.github.com>
2020-05-20 12:34:43 +0200

    Add files via upload

---
+++ to-do list/mainpage.js
@@ -0,0 +1,8 @@
+var popupDiv = document.getElementById("popup_background");
+
+function openPopup(){
+    popupDiv.style.display = "block";
+}
+function closePopup(){
+    popupDiv.style.display = "none";
+}
---
+++ to-do list/newTask.js
@@ -0,0 +1,4 @@
+const member = document.querySelector("[name='name']").value;
+const date = document.querySelector("[type='date']").value;
+const priority = document.querySelector("[type='color']").value;
+const description = document.querySelector("[name='description']");
