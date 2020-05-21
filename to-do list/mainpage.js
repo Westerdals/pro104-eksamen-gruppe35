0fc367daa9a761857e835b236f4cc66b4b62aa38
1drees <1drees>
2020-05-21 06:21:35 +0200

    lagt til newMember - local storage funker ikke enda

--- to-do list/mainpage.js
+++ to-do list/mainpage.js
@@ -1,8 +1,16 @@
 var popupDiv = document.getElementById("popup_background");
+var memberPopupDiv = document.getElementById("popup_member_background");
 
 function openPopup(){
     popupDiv.style.display = "block";
 }
 function closePopup(){
     popupDiv.style.display = "none";
+}
+
+function openMemberPopup(){
+    memberPopupDiv.style.display = "block";
+}
+function closeMemberPopup(){
+    memberPopupDiv.style.display = "none";
 }
